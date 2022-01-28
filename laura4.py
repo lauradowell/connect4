fe66e5b1ac13009f581f77aca70171bdbfff7331
lauradowell <95287786+lauradowell@users.noreply.github.com>
2022-01-28 17:28:32 +0100

    Add files via upload

--- laura4.py
+++ laura4.py
@@ -5,8 +5,8 @@ P1 = str(input('Player1, enter your name: '))
 
 P2 = str(input('Player2, enter your name: '))
 
-player1 = { 1: P1, 2: "|x|" }
-player2 = { 1: P2, 2: "|o|"}
+player1 = { 1: P1, 2: 'x' }
+player2 = { 1: P2, 2: 'o'}
 
 
 #print("Hello, " + P1 + " and " + P2 + "!"+ " Welcome to Connect 4")
@@ -41,6 +41,12 @@ emptymap = [
     "————————————————————————————— ",
 ]
 
+row1 = [1, 2, 3, 4, 5, 6, 7]
+row2 = [1, 2, 3, 4, 5, 6, 7]
+row3 = [1, 2, 3, 4, 5, 6, 7]
+row4 = [1, 2, 3, 4, 5, 6, 7]
+row5 = [1, 2, 3, 4, 5, 6, 7]
+row6 = [1, 2, 3, 4, 5, 6, 7]
 
 chiplog= []
 
@@ -52,7 +58,7 @@ def countachip(currentPlayer):
         #currentPlayer[1] accesses name, Currentplayer[2]" accesses chip x or o
         UI1 = input(str(currentPlayer[1]) + ', Enter a row number to put down your chip: ')
         isuiadigit = UI1.isdigit()
-        print(isuiadigit)
+        
         if isuiadigit == True :
             taketurn = True
         else: print(str(currentPlayer[1]) + ", That wasn't a number.")
@@ -67,8 +73,8 @@ def countachip(currentPlayer):
                 columns[UI1] -= 1
                 chippos = UI1 , columns[UI1]
                 chiplog.insert(1, chippos)
-                print(chiplog)
-                print (chiplog[0], "hi")
+                #print(chiplog)
+                #print (chiplog[0], "hi")
 
                 
                 print("X pos is ", UI1, "Y pos is", columns[UI1]) 
@@ -82,27 +88,102 @@ def countachip(currentPlayer):
         UI1 = input(str(currentPlayer[1])  + ', That is not a number. Enter a row number to put down your chip: ')
     
      
+    rowok = {
+    # column : counter
+    -6: row6,
+    -5: row5,
+    -4: row4,
+    -3: row3,
+    -2: row2,
+    -1: row1,
+    
+    #row7: 0,
+    }
+    
+  
+            
+   
+    for key in  rowok:  
+        #if y  pos == a number in our list of row names
+        if columns[UI1] == key :
+            #acess rowname with key, print row1
+
+
+                for i in rowok[key]:
+                    chipHasBeenPlaced = rowok[key]
+                    #oos =  print(" O", end  = (""))
+  
+                    if i == UI1:
+                        for  keys, values in  rowok.items():
+                                del chipHasBeenPlaced [UI1-1]
+                                chipHasBeenPlaced.insert(UI1-1, currentPlayer[2])
+                                print(values)
+                                
+                    else:
+                         for  keys, values in  rowok.items():
+                            slice1  =  slice(-7,UI1-1)
+                           # print("sl", slice1)
+                            slice2 =  slice
+                            #del chipHasBeenPlaced [values]
+                            #w
+                            # chipHasBeenPlaced.insert(slice1, "O")
+                            #print(values)
+                             
+                        
+            
+     #next  step:: how to  translate   my list info(rowok) into a grid
+
+    
+    
+            #print (values)
+            #print ("|  ", end = "|")
+            
+
+        for i in range(7):
+            print ("")
+            for e in range(7):
+                print ("O",  end = "")
+                for keys, values in rowok.items():
+                    for nums in values:
+                        if nums == (2):
+                            print ("H",  end = "")
+
+
     #for i in chiplog:
          #for j in i:
              #print (i[0], i[1], "yeiie")
     
-    for i in range(7):
-        print("")
-        for j in range(7):
+    #for i in range(7):
+     #   print("")
+      #  for j in range(7):
             
-            for b in chiplog:
+      #      for b in chiplog:
 
-                if i == b[0]: #and j == b[1]:
+       #         if i == b[0]: #and j == b[1]:
                      
-                     print(currentPlayer[2], end =  "")
+        #             print(currentPlayer[2], end =  "")
             
-            print ("| |", end = "")
+        #    print ("| |", end = "")
                  
-    print("")  
+    #print("")  
+
+    
+   
+   
+    
+    #for x in range (7):
+      #  print("e")
+       # for  y in range (7):
+            #print ("|a|", end = "")
+           # print("w",  end = "")
+            #print("")
+                
+                #if x == chiplog[0] and y == chiplog[1]:
+                   # print ("|X|", end = "")
+           
+
 
                 
-                 
-
 while true == True:
      countachip(player1)
      countachip(player2)
